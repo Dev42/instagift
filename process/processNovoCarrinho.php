78716396c3816ef4e15cd72f75629304631dd819
André Simões <afsdede92@gmail.com>
2013-04-16 02:29:54 -0300

    Adição do carrinho mais forma de pagamento

---
+++ process/processNovoCarrinho.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+include_once '../painel/conf/classLoader.php';
+include_once '../config/connection.php';
+include_once '../processaPagamento.php';
+
+$chart = new Chart();
+$ped = new Pedido();
+$pedController = new PedidosController();
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+$chart->setCliId($_SESSION['IdInstagift']);
+$chart->setPrdId($_POST['prdId']);
+$chart->setPrdConfig("");
+$chart->setQuantidade(1);
+$chart->setCreatedAt(time());
+
+$qry = "INSERT INTO `instagift`.`pedidos_chart` (`cht_10_id`, `user_10_id`, `produto_10_id`, `cht_35_config`, `cht_10_quantidade`, `cht_22_created_at`) VALUES ('1', '".$chart->getCliId()."', '".$chart->getPrdId()."', '', '1', '".$chart->getCreatedAt()."');";
+echo $qry;
+mysql_query($qry);
+$chartId = mysql_insert_id();
+
+$ped->setChartId($chartId);
+$ped->setStatus(1);
+$ped->setStatusPag(1);
+$ped->setPayMode("PAGSEGURO");
+$ped->setCreatedAt(time());
+
+if ($pedController->insertAction($ped)){
+    echo "Pedido Criado Com Sucesso!";
+}
+
+$procPag = new processaPagamento($chart);
+
+?>
