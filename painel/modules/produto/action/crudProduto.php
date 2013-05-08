ba0ce965c5d8ae39585ce23aaa14206cebcf1b80
André Simões <afsdede92@gmail.com>
2013-05-07 22:34:03 -0300

    Alterações para receber a informações clonaveis de produto

--- painel/modules/produto/action/crudProduto.php
+++ painel/modules/produto/action/crudProduto.php
@@ -30,7 +30,7 @@ switch ($op) {
                 }
             }
 
-            if ($nome != "" && $descCurta != "" && $descCompleta != "" && $valor != "" && $peso != "" && $prazoProducao != "" && $larguraMinima != "" && $alturaMinima != "" && $minimoFotos != "" && count($prd_forn) > 0 && $prd_image["name"] != "") {
+            if ($nome != "" && $descCurta != "" && $descCompleta != "" && $prazoProducao != "" && $larguraMinima != "" && $alturaMinima != "" && $minimoFotos != "" && count($prd_forn) > 0 && $prd_image["name"] != "") {
 
                 $produtoClass = new Produto();
                 $produtoController = new ProdutoController();
@@ -38,8 +38,6 @@ switch ($op) {
                 $produtoClass->setNome($nome);
                 $produtoClass->setDescCurta($descCurta);
                 $produtoClass->setDescCompleta($descCompleta);
-                $produtoClass->setValor(str_replace(",", ".", $valor));
-                $produtoClass->setPeso(str_replace(",", ".", $peso));
                 $produtoClass->setPrazoProducao($prazoProducao);
                 $produtoClass->setLarguraMinima($larguraMinima);
                 $produtoClass->setAlturaMinima($alturaMinima);
@@ -57,7 +55,6 @@ switch ($op) {
                 $enc = json_encode($arColor);
                 
                 $produtoClass->setCores($enc);
-                $produtoClass->setFrete(str_replace(",", ".", $frete));
                 
                 if (array_key_exists("name", $prd_banner)) {
                     $produtoClass->setUrl($prd_banner);
@@ -155,7 +152,7 @@ switch ($op) {
                     }
                 }
             }
-            if ($produto_10_id != "" && $nome != "" && $descCurta != "" && $descCompleta != "" && $valor != "" && $frete != "" && $peso != "" && $prazoProducao != "" && $larguraMinima != "" && $alturaMinima != "" && $minimoFotos != "" && count($prd_forn) > 0) {
+            if ($produto_10_id != "" && $nome != "" && $descCurta != "" && $descCompleta != "" && $prazoProducao != "" && $larguraMinima != "" && $alturaMinima != "" && $minimoFotos != "" && count($prd_forn) > 0) {
 
                 $produtoClass = new Produto();
                 $produtoController = new ProdutoController();
@@ -164,7 +161,6 @@ switch ($op) {
                 $produtoClass->setNome($nome);
                 $produtoClass->setDescCurta($descCurta);
                 $produtoClass->setDescCompleta($descCompleta);
-                $produtoClass->setValor(str_replace(",", ".", $valor));
                 
                 $arColor = array();
                 $controleCor = 0;
@@ -178,8 +174,6 @@ switch ($op) {
                 $enc = json_encode($arColor);
                 
                 $produtoClass->setCores($enc);
-                $produtoClass->setFrete(str_replace(",", ".", $frete));
-                $produtoClass->setPeso(str_replace(",", ".", $peso));
                 $produtoClass->setPrazoProducao($prazoProducao);
                 $produtoClass->setLarguraMinima($larguraMinima);
                 $produtoClass->setAlturaMinima($alturaMinima);
---
+++ painel/modules/produto/entity/ProdutoInfo.php
@@ -0,0 +1,98 @@
+<?php
+
+class ProdutoInfo {
+    
+    protected $id;
+    
+    protected $idProduto;
+    
+    protected $nome;
+    
+    protected $valor;
+    
+    protected $desc;
+    
+    protected $peso;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getIdProduto() {
+        return $this->idProduto;
+    }
+
+    public function setIdProduto($idProduto) {
+        $this->idProduto = $idProduto;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getValor() {
+        return $this->valor;
+    }
+
+    public function setValor($valor) {
+        $this->valor = $valor;
+    }
+
+    public function getDesc() {
+        return $this->desc;
+    }
+
+    public function setDesc($desc) {
+        $this->desc = $desc;
+    }
+
+    public function getPeso() {
+        return $this->peso;
+    }
+
+    public function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    public function assocEntity(){
+        
+        $fields = array(
+            "produto_info_10_id"            => $this->getId(),
+            "produto_10_id"                 => $this->getIdProduto(),
+            "produto_info_30_nome"          => $this->getNome(),
+            "produto_info_20_valor"         => $this->getValor(),
+            "produto_info_35_desc"          => $this->getDesc(),
+            "produto_info_12_peso"          => $this->getPeso()
+        );
+        
+        return $fields;
+        
+    }
+    
+    public function fetchEntity($row){
+        
+        $this->setId($row['produto_info_10_id']);
+        $this->setIdProduto($row['produto_10_id']);
+        $this->setNome($row['produto_info_30_nome']);
+        $this->setValor($row['produto_info_20_valor']);
+        $this->setDesc($row['produto_info_35_desc']);
+        $this->setPeso($row['produto_info_12_peso']);
+        
+        return $this;
+    }
+    
+    public function tableName(){
+        return "produto_info";
+    }
+    
+}
+
+?>
